3db2319704c1e6c53587a6fb3326f1aca5de6ab2
Rick Waldron <waldron.rick@gmail.com>
2011-07-21 15:50:19 -0400

    Updates to match jQuery Core Style Guide

--- jquery.cookie.js
+++ jquery.cookie.js
@@ -7,35 +7,42 @@
  * http://www.gnu.org/licenses/gpl.html
  *
  */
-jQuery.cookie = function (key, value, options) {
+(function( global, jQuery ) {
+	jQuery.cookie = function( key, value, options ) {
 
-    // key and at least value given, set cookie...
-    if (arguments.length > 1 && String(value) !== "[object Object]") {
-        options = jQuery.extend({}, options);
+		var days, time, result, decode;
+		// key and at least value given, set cookie...
+		if ( arguments.length > 1 && jQuery.type(value) !== "object" ) {
+			options = jQuery.extend({}, options);
 
-        if (value === null || value === undefined) {
-            options.expires = -1;
-        }
+			if ( value == null ) {
+				options.expires = -1;
+			}
 
-        if (typeof options.expires === 'number') {
-            var days = options.expires, t = options.expires = new Date();
-            t.setDate(t.getDate() + days);
-        }
+			if ( options.expires && jQuery.type(options.expires) === "number" ) {
 
-        value = String(value);
+				days = options.expires;
+				time = options.expires = new Date();
+				time.setDate( time.getDate() + days );
+			}
 
-        return (document.cookie = [
-            encodeURIComponent(key), '=',
-            options.raw ? value : encodeURIComponent(value),
-            options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-            options.path ? '; path=' + options.path : '',
-            options.domain ? '; domain=' + options.domain : '',
-            options.secure ? '; secure' : ''
-        ].join(''));
-    }
+			value = String(value);
 
-    // key and possibly options given, get cookie...
-    options = value || {};
-    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
-    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
-};
+			return (document.cookie = [
+				encodeURIComponent(key), "=",
+				options.raw ? value : encodeURIComponent(value),
+				// use expires attribute, max-age is not supported by IE
+				options.expires ? "; expires=" + options.expires.toUTCString() : "",
+				options.path ? "; path=" + options.path : "",
+				options.domain ? "; domain=" + options.domain : "",
+				options.secure ? "; secure" : ""
+			].join(""));
+		}
+
+		// key and possibly options given, get cookie...
+		options = value || {};
+		decode = options.raw ? function(s) { return s; } : decodeURIComponent;
+
+		return (result = new RegExp("(?:^|; )" + encodeURIComponent(key) + "=([^;]*)").exec(document.cookie)) ? decode(result[1]) : null;
+	};
+})( this, this.jQuery );
